d651828673b580e2819d2d795be17b1a4658e5cc
Yuvraj <singyuvraj65035@gmail.com>
2026-04-20 15:19:06 +0530

    added the logger in the allure listener

--- src/test/java/listeners/AllureListeners.java
+++ src/test/java/listeners/AllureListeners.java
@@ -2,6 +2,8 @@ package listeners;
 
 import base.DriverFactory;
 import io.qameta.allure.Allure;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -14,15 +16,17 @@ import java.io.ByteArrayInputStream;
 
 public class AllureListeners implements ITestListener, IInvokedMethodListener {
 
+    private static final Logger logger = LogManager.getLogger(AllureListeners.class);
     @Override
     public void afterInvocation(IInvokedMethod method , ITestResult iTestResult) {
         if(method.isTestMethod() && iTestResult.getStatus() == ITestResult.FAILURE) {
-            System.out.println("Test failed. Capturing screenshot in afterInvocation...");
+
+            logger.info("Test failed. Capturing screenshot in afterInvocation...");
             WebDriver driver = DriverFactory.getDriver();
             if(driver != null) {
                 Allure.addAttachment(iTestResult.getMethod().getMethodName() + " - Failed Screenshot",
                         new ByteArrayInputStream(((TakesScreenshot) DriverFactory.getDriver()).getScreenshotAs(OutputType.BYTES)));
-                System.out.println("Screenshot successfully attached to Allure!");
+                logger.info("Screenshot successfully attached to Allure!");
             }
         }
     }
